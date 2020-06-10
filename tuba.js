7e73728c4ee9f621e1bd5fe31980f5821225fca6
bren (SmatMan) <gamingajun10@gmail.com>
2020-06-10 11:56:05 -0400

    fix integer error

--- tuba.js
+++ tuba.js
@@ -1,7 +1,7 @@
-
+var clicks = 0;
 function play(){
-    var audio = document.getElementById("audio");
-    audio.play();
-    clicks += 1;
-    document.getElementById("clicks").innerHTML = clicks;
+     var audio = document.getElementById("audio");
+     audio.play();
+     clicks += 1;
+     document.getElementById("clicks").innerHTML = clicks;
 }
